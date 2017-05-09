37d45e7975c76d684752717b748ce2da0584d9e2
Kevin Hickey <kevin.hickey@aberdeen-asset.com>
2017-05-09 20:50:34 +1000

    NowWorking

--- MediatorDemo/MainWindow.xaml.cs
+++ MediatorDemo/MainWindow.xaml.cs
@@ -23,6 +23,26 @@ namespace MediatorDemo
         public MainWindow()
         {
             InitializeComponent();
+            LoadControls();
         }
+
+
+        private void LoadControls()
+        {
+            var jobs = new Jobs();
+            jobs.SetValue(Grid.RowProperty, 1);
+            LayoutRoot.Children.Add(jobs);
+
+            var empsOnJob = new EmployeesOnJob();
+            var jobDetails = new JobDetails();
+            jobDetails.Margin = new Thickness(25, 0, 0, 0);
+            jobDetails.SetValue(Grid.ColumnProperty, 1);
+
+            ContainerGrid.Children.Add(empsOnJob);
+            ContainerGrid.Children.Add(jobDetails);
+        }
+
     }
+
+
 }
---
+++ MediatorDemo/UserControls/JobDetails.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+
+namespace MediatorDemo
+{
+    /// <summary>
+    /// Interaction logic for JobDetails.xaml
+    /// </summary>
+    public partial class JobDetails : UserControl
+    {
+        public JobDetails()
+        {
+            InitializeComponent();
+            Mediator.GetInstance().JobChanged += (s, e) =>
+            {
+                this.DataContext = e.Job;
+            };
+        }
+
+    }
+}
